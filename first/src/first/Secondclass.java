b45d06b12d2517f8575604046e156ab3d6c8afdb
tejasvipe <tejasvipenmetsa1@gmail.com>
2019-03-03 20:20:45 -0500

    Revert "added third line"
    This reverts commit f5f0ca705e6972572a05de826fbff473cf36cee8.

--- first/src/first/Secondclass.java
+++ first/src/first/Secondclass.java
@@ -4,7 +4,6 @@ public class Secondclass {
     public static void main(String[] args) {
     	System.out.println("hie"); 
     	System.out.println("hello");
-    	System.out.println("how");
     }
 	
 
